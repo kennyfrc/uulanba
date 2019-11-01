ad9c64ead9f3e9654b6f7552574b76653f90286c
kennyfrc <kennyfrc@Kenns-MacBook-Pro.local>
2019-11-02 02:41:01 +0800

    added sitemap

---
+++ config/sitemap.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'sitemap_generator'
+
+SitemapGenerator::Sitemap.default_host = 'https://uulanba.com'
+SitemapGenerator::Sitemap.create do
+  add '/', :changefreq => 'daily', :priority => 0.9
+  add '/Manila', :changefreq => 'daily'
+  add '/Taguig', :changefreq => 'daily'
+  add '/Makati', :changefreq => 'daily'
+  add '/Pasig', :changefreq => 'daily'
+  add '/Pasay', :changefreq => 'daily'
+end
+SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
